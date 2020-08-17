7bc1c40d379a3b59f2beac4cc399d87718e4f652
William O'Brien <wiobrien@calpoly.edu>
2020-08-16 19:05:45 -0700

    Got the basic navigation structure set

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import { Feed } from './pages/feed/feed';
 import { AllFish } from './pages/allfish/allfish';
 import { LeaderBoards } from './pages/leaderboards/leaderboards';
 import { Settings } from './pages/settings/settings';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 // Navigator types, used for title bar and for tabs
 const Tab = createMaterialBottomTabNavigator();
@@ -16,16 +18,47 @@ const Stack = createStackNavigator();
 // Tabs are cool
 function TabBar() {
   return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      activeColor="#f0edf6"
-      inactiveColor="#3e2465"
-      barStyle={{ backgroundColor: '#694fad' }}
-    >
-      <Tab.Screen name="Home" component={Feed} />
-      <Tab.Screen name="All Fish" component={AllFish} />
-      <Tab.Screen name="Leaders" component={LeaderBoards} />
-      <Tab.Screen name="Settings" component={Settings} />
+    <Tab.Navigator initialRouteName="Home" shifting={true}>
+      <Tab.Screen
+        name="Home"
+        options={{
+          tabBarColor: '#a0a6e2',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="md-home" color="white" size={26} />
+          ),
+        }}
+        component={Feed}
+      />
+      <Tab.Screen
+        name="All Fish"
+        options={{
+          tabBarColor: '#8a84ce',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="fish" color="white" size={26} />
+          ),
+        }}
+        component={AllFish}
+      />
+      <Tab.Screen
+        name="Leaders"
+        options={{
+          tabBarColor: '#ff9391',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="md-trophy" color="white" size={26} />
+          ),
+        }}
+        component={LeaderBoards}
+      />
+      <Tab.Screen
+        name="Settings"
+        options={{
+          tabBarColor: '#ffb1c1',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="md-settings" color="white" size={26} />
+          ),
+        }}
+        component={Settings}
+      />
     </Tab.Navigator>
   );
 }
