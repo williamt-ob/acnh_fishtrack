16d01ec364bfff05fc43d10cc9ab13f08b33062c
William O'Brien <wiobrien@calpoly.edu>
2020-08-18 17:51:02 -0700

    Added a basic filter option to the all fish category

--- pages/allfish/allfish.js
+++ pages/allfish/allfish.js
@@ -1,15 +1,27 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useState } from 'react';
 import { fishData } from '../../data/fishdata';
-import { StyleSheet, Text, View } from 'react-native';
+import { Picker, StyleSheet, Text, View } from 'react-native';
+
+const sortOptions = ['Value', 'Name', 'Catchable'];
 
 export const AllFish = () => {
+  const [sortBy, setSortBy] = useState('Name');
   console.log(fishData);
   return (
     <>
-      {Object.keys(fishData).map((key) =>
-        <Text key={key }>{fishData[key].name}</Text>
-      )}
+      <Picker
+        selectedValue={sortBy}
+        style={{ height: 50, width: 150 }}
+        onValueChange={(itemValue, itemIndex) => setSortBy(itemValue)}
+      >
+        {sortOptions.map((option) => (
+          <Picker.Item key={option} label={option} value={option} />
+        ))}
+      </Picker>
+      {Object.keys(fishData).map((key) => (
+        <Text key={key}>{fishData[key].name}</Text>
+      ))}
     </>
   );
 };
