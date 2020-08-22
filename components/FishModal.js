5dc7fdaa4aa6244a664d3b55cd1757ee06fd8ec7
William O'Brien <wiobrien@calpoly.edu>
2020-08-22 13:28:11 -0700

    Added a few other headers that definitely need to be styled better, also added some more TODOs for future work

--- components/FishModal.js
+++ components/FishModal.js
@@ -11,19 +11,40 @@ import {
 
 //TODO: get the back button overwritten to run handleClose
 
-export const FishModal = ({ fishData, open, handleClose,...rest }) => {
+export const FishModal = ({ fishData, open, handleClose, ...rest }) => {
   return (
     <Modal style={styles.entryView} visible={open} onBack={handleClose}>
-      <TouchableHighlight>
-        <Text>{fishData.name}</Text>
-      </TouchableHighlight>
+      <Text style={styles.fishTitle}>{fishData.name}</Text>
+      <Text style={styles.fishHeader}>{fishData.location}</Text>
+      <Text
+        style={styles.fishHeader}
+      >{`${fishData.shadowSize} Shadow Size`}</Text>
+      <Text style={styles.fishHeader}>{`Catchable: ${fishData.time}`}</Text>
+      <Text
+        style={styles.fishHeader}
+      >{`Northern Hemisphere Dates: ${fishData.northDate}`}</Text>
+      <Text
+        style={styles.fishHeader}
+      >{`Southern Hemisphere Dates: ${fishData.southDate}`}</Text>
       <Button title="Close" onPress={handleClose} />
+      {/* TODO: add an image of the fish location on a map */}
+      {/* TODO: add an image of the fish relative shadow size */}
+
     </Modal>
   );
 };
 
+//TODO: improve the styling of the title/headers, find good examples
+
 const styles = StyleSheet.create({
   entryView: {
     flexDirection: 'row',
+  },
+  fishTitle: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  fishHeader: {
+    fontSize: 16,
   },
 });
