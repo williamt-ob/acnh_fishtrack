98f48a27d0179def9601a4f8e2f7a396af1161ad
William O'Brien <wiobrien@calpoly.edu>
2020-08-22 14:15:11 -0700

    Added more function headers and some pseudocode for the time calculations

--- common/catchablenow.js
+++ common/catchablenow.js
@@ -1,9 +1,37 @@
-const _catchableToday = (fishData) => {
+const _dateNumToMonth = (dateNum) => {
+  switch (dateNum) {
+    case 0:
+      return 'January';
+    case 1:
+      return 'February';
+    case 2:
+      return 'March';
+    case 3:
+      return 'April';
+    default:
+      return 'Invalid date';
+  }
+};
+
+const _isCatchableToday = (fishData, today) => {
   return false;
 };
 
-const _catchableNow = (fishData) => {
+const _isCatchableRightNow = (fishData, today) => {
   return false;
 };
 
-export const isCatchableNow = (fishData) => {};
+export const isCatchable = (fishData) => {
+  const today = new Date();
+
+  const catchableToday = _isCatchableToday(fishData, today);
+  // Only check now if today is good, otherwise disregard
+  const catchableNow = catchableToday
+    ? _isCatchableRightNow(fishData, today)
+    : false;
+
+  return {
+    catchableNow,
+    catchableToday,
+  };
+};
