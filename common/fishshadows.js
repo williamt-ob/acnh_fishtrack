32c691c37dc2b2ec6024f1c25a3e20d8030f2a26
William O'Brien <wiobrien@calpoly.edu>
2020-08-23 09:56:45 -0700

    Added in the require statements for the images

---
+++ common/fishshadows.js
@@ -0,0 +1,10 @@
+import { fishSizes } from './fishsizes';
+
+export const fishShadows = {
+  Smallest: require('../assets/shadows/fishshadows/Smallest.png'),
+  Small: require('../assets/shadows/fishshadows/Small.png'),
+  Medium: require('../assets/shadows/fishshadows/Medium.png'),
+  Large: require('../assets/shadows/fishshadows/Large.png'),
+  'X Large': require('../assets/shadows/fishshadows/X Large.png'),
+  Largest: require('../assets/shadows/fishshadows/Largest.png'),
+};
--- common/fishsizes.js
+++ common/fishsizes.js
@@ -5,7 +5,9 @@ export const fishSizes = [
   'Large',
   'X Large',
   'Largest',
-  'Narrow',
+  //'Narrow',
 ];
 
 //TODO: fix data validation, blanks, and why one of them is just '(Fin)'
+
+//TODO: figure out how to handle the narrow category
